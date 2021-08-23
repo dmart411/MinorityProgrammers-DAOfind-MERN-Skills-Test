fd802e46797c1ace2815aef7376dd8a1e4697beb
dmart411 <dmart411@fiu.edu>
2021-08-23 12:57:43 -0400

    react router set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div>DAO Find</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">DAO Find</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
