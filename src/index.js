ac5d17a08fe1a9f9011c269e41afae63d96cf162
dmart411 <dmart411@fiu.edu>
2021-08-23 12:54:04 -0400

    initial redux store setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createStore, compose } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  daos: (state = [], action) => {
+    return state;
+  },
+});
