b2beb4e16e0c67a77149a4828a977c55610b91d7
dmart411 <dmart411@fiu.edu>
2021-08-23 22:15:56 -0400

    added crud action creators for dao endpoint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,38 @@
-import { ADD_DAO } from "./types";
+import axios from "axios";
+import api from "../api";
+import { ADD_DAO, FETCH_DAOS, FETCH_DAO, UPDATE_DAO } from "./types";
 
-export const addDao = (dao) => {
+export const fetchDaos = () => async (dispatch) => {
+  const res = await axios.get("/daos");
   return {
-    type: ADD_DAO,
-    payload: dao,
+    type: FETCH_DAOS,
+    payload: res.data,
   };
 };
+
+export const fetchDao = (id) => async (dispatch) => {
+  const res = await axios.get(`/daos/${id}`);
+  return {
+    type: FETCH_DAO,
+    payload: res.data,
+  };
+
+}
+
+export const updateDao = (dao) => async (dispatch) => {
+  const res = await axios.patch(`/daos/${dao.id}`, dao);
+  return {
+    type: UPDATE_DAO,
+    payload: res.data,
+  };
+}
+
+export const addDao = (dao) => async (dispatch) => {
+  const res = await axios.post("/daos", dao);
+  return {
+    type: ADD_DAO,
+    payload: res.data,
+  };
+};
+
+
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,4 @@
 export const ADD_DAO = "ADD_DAO";
 export const UPDATE_DAO = "UPDATE_DAO";
+export const FETCH_DAO = "FETCH_DAO";
+export const FETCH_DAOS = "FETCH_DAOS";
