3bdbeaeb4b0fce5696e03a272fd575f0117c1c1d
dmart411 <dmart411@fiu.edu>
2021-08-24 15:54:23 -0400

    dao redux store update

--- src/reducers/daoReducer.js
+++ src/reducers/daoReducer.js
@@ -2,7 +2,7 @@ import _ from "lodash";
 import { ADD_DAO, FETCH_DAO, FETCH_DAOS, UPDATE_DAO } from "../actions/types";
 
 const daoReducer = (state = [], action) => {
-  switch (action.type) {
+   switch (action.type) {
     case FETCH_DAO:
       return _.uniqBy([action.payload, ...state], "id");
     case FETCH_DAOS:
