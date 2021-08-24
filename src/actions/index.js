450c8614f5d4307a900d0bf9a27ca848f5f77a91
dmart411 <dmart411@fiu.edu>
2021-08-24 15:55:21 -0400

    updated dao action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,35 +3,32 @@ import { ADD_DAO, FETCH_DAOS, FETCH_DAO, UPDATE_DAO } from "./types";
 
 export const fetchDaos = () => async (dispatch) => {
   const res = await api.get("/daos");
-  return {
+  dispatch({
     type: FETCH_DAOS,
     payload: res.data,
-  };
+  });
 };
 
 export const fetchDao = (id) => async (dispatch) => {
-  const res = await api.get(`{}/daos/${id}`);
-  return {
+  const res = await api.get(`/daos/${id}`);
+  dispatch({
     type: FETCH_DAO,
     payload: res.data,
-  };
-
-}
+  });
+};
 
 export const updateDao = (dao) => async (dispatch) => {
   const res = await api.patch(`/daos/${dao.id}`, dao);
-  return {
+  dispatch({
     type: UPDATE_DAO,
     payload: res.data,
-  };
-}
+  });
+};
 
 export const addDao = (dao) => async (dispatch) => {
   const res = await api.post("/daos", dao);
-  return {
+  dispatch({
     type: ADD_DAO,
     payload: res.data,
-  };
+  });
 };
-
-
