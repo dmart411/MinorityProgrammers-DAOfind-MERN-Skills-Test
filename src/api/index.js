3cd7d8ec3804f81dd3cd56226ad2b210454d2498
dmart411 <dmart411@fiu.edu>
2021-08-24 15:52:37 -0400

    updated endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://https://dao-json-server.herokuapp.com/",
+  baseURL: "https://dao-json-server.herokuapp.com/",
 });
 
 export default instance;
