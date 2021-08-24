d9c96723b0a7a09c41c344111132153c779107d2
dmart411 <dmart411@fiu.edu>
2021-08-23 22:26:28 -0400

    implemented dao reducer

--- src/reducers/daoReducer.js
+++ src/reducers/daoReducer.js
@@ -1,7 +1,14 @@
-import { ADD_DAO } from "../actions/types";
+import _ from "lodash";
+import { ADD_DAO, FETCH_DAO, FETCH_DAOS, UPDATE_DAO } from "../actions/types";
 
 const daoReducer = (state = [], action) => {
   switch (action.type) {
+    case FETCH_DAO:
+      return _.uniqBy([action.payload, ...state], "id");
+    case FETCH_DAOS:
+      return action.payload;
+    case UPDATE_DAO:
+      return _.uniqBy([action.payload, ...state], "id");;
     case ADD_DAO:
       return [...state, action.payload];
     default:
